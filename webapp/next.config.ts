4afe8172bc66e8cb1cd1d2d3e5db396f38fac3a7
Luciano Nooijen <luciano@lucianonooijen.com>
2024-11-09 04:26:57 +0100

    ci(webapp): standalone build
    Signed-off-by: Luciano Nooijen <luciano@lucianonooijen.com>

--- webapp/next.config.ts
+++ webapp/next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    /* config options here */
+    output: "standalone",
+    productionBrowserSourceMaps: true,
+    reactStrictMode: true,
+    swcMinify: true,
 };
 
 export default nextConfig;
