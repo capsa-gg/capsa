5cd088dbff9162d226fe5668cd13bf210db8f4b5
Luciano Nooijen <luciano@lucianonooijen.com>
2024-11-15 02:05:58 +0100

    feat(server): add log chunk processing logic with unit tests
    Signed-off-by: Luciano Nooijen <luciano@lucianonooijen.com>

---
+++ server/internal/domain/logs/process_chunk.go
@@ -0,0 +1,222 @@
+package logs
+
+import (
+	"errors"
+	"time"
+
+	"go.uber.org/zap"
+)
+
+var (
+	errorLineEmpty                    = errors.New("line is empty")
+	errorLineNoValidStart             = errors.New("character is not a valid start of a line, required: '['")
+	errorLineInvalidMetadataChar      = errors.New("metadata parsing failed due to invalid character")
+	errorLineUnexpectedEnd            = errors.New("metadata parsing failed due to unexpected end of data")
+	errorLineEmptyMetadataBlock       = errors.New("metadata parsing failed due to empty metadata block")
+	errorLineInvalidMetadataString    = errors.New("metadata blocks should have no characters between them")
+	errorLineTimestampParsing         = errors.New("timestamp cannot be parsed due to incorrect formatting")
+	errorLineMissingEndOfMetadataChar = errors.New("metadata blocks should be ended with a `:` character")
+)
+
+const timestampParseLayout = "2006.01.02-15.04.05.999"
+
+type logChunkLineMetadata struct {
+	Timestamp time.Time
+	Category  string
+	Severity  string
+}
+
+func (l logChunkLineMetadata) isComplete() bool {
+	return l.Severity != "" && l.Category != "" && !l.Timestamp.IsZero()
+}
+
+type logChunkMetadata struct {
+	Start           time.Time
+	End             time.Time
+	LineCount       int
+	CategoriesCount map[string]int // Used to get a string[] of categories
+	SeveritiesCount map[string]int
+}
+
+// Should only be called when the line processing was successful and the line .isComplete().
+func (lcm *logChunkMetadata) addLineMetadata(lineMetadata logChunkLineMetadata) {
+	if lcm.Start.IsZero() || lineMetadata.Timestamp.Before(lcm.Start) {
+		lcm.Start = lineMetadata.Timestamp
+	}
+
+	if lineMetadata.Timestamp.After(lcm.End) {
+		lcm.End = lineMetadata.Timestamp
+	}
+
+	lcm.CategoriesCount[lineMetadata.Category]++
+	lcm.SeveritiesCount[lineMetadata.Severity]++
+}
+
+// extractMetadataFromChunk takes in a log chunk as []byte and returns the metadata,
+// the unprocessed/unprocessable lines are logged to logger.Warnf() as well as returned to the caller.
+func extractMetadataFromChunk(logger *zap.SugaredLogger, logChunk []byte) (logChunkMetadata, []string) { //nolint:gocritic // This is fine and documented in a comment
+	log := logger.Named("extractMetadataFromChunk").Desugar() // Desugar for a bit better performance
+	unprocessedLines := []string{}
+	chunkMetadata := logChunkMetadata{
+		CategoriesCount: map[string]int{},
+		SeveritiesCount: map[string]int{},
+	}
+
+	lineContents := make([]byte, 0, 512)
+	lineCount := 0
+
+	// Loop over the chunk in O(1), when we find a \n character, we handle the line
+	for i, c := range logChunk {
+		// If we don't encounter a new line, add to lineContents and continue loop
+		if c != '\n' {
+			lineContents = append(lineContents, c)
+
+			continue
+		}
+
+		// We encountered a \n, so handle the line and try and get the metadata for it
+		lineCount++
+
+		lineMetadata, err := extractMetadataFromLine(lineContents)
+
+		switch {
+		case err != nil: // Metadata is complete and should be processed
+			log.With(
+				zap.Int("line", i),
+				zap.ByteString("lineContents", lineContents),
+				zap.Error(err),
+			).Warn("error extracting metadata from line, not processing")
+
+			unprocessedLines = append(unprocessedLines, string(lineContents))
+		case !lineMetadata.isComplete(): // Don't process incomplete lines
+			log.With(
+				zap.Int("line", i),
+				zap.Time("timestamp", lineMetadata.Timestamp),
+				zap.String("severity", lineMetadata.Severity),
+				zap.String("category", lineMetadata.Severity),
+				zap.ByteString("lineContents", lineContents),
+				zap.Error(err),
+			).Warn("metadata from line is not complete, not processing")
+
+			unprocessedLines = append(unprocessedLines, string(lineContents))
+		default:
+			chunkMetadata.addLineMetadata(lineMetadata)
+		}
+
+		// Reset lineContents
+		lineContents = make([]byte, 0, 1024)
+	}
+
+	// Set the total line count, then return data
+	chunkMetadata.LineCount = lineCount
+
+	return chunkMetadata, unprocessedLines
+}
+
+//nolint:funlen,nakedret,gocognit,gocyclo // This is easier to comprehend as a single function with naked returns, the function is somewhat complex, but hard to simplify, the alternative is a lexer/parser
+func extractMetadataFromLine(line []byte) (lineMetadata logChunkLineMetadata, err error) {
+	// Check if we have content
+	if len(line) == 0 {
+		err = errorLineEmpty
+
+		return
+	}
+
+	// Lines must start with a `[` to be considered valid
+	if line[0] != '[' {
+		err = errorLineNoValidStart
+
+		return
+	}
+
+	isInMetadataBlock := false
+	metadataBlockCount := 0                      // We use 1-based indexing for this in the logic below
+	metadataBlockContents := make([]byte, 0, 64) // Assuming we don't go over 64 characters
+
+	// Loop over characters, not using regex for performance
+charLoop:
+	for i, char := range line {
+		hasNext := len(line) > i+1
+
+		if char == '[' {
+			// If we get `[` without a `]` first, we cannot parse the metadata correctly: failing example: `[Timestamp][[Severity]`
+			if isInMetadataBlock {
+				err = errorLineInvalidMetadataChar
+
+				return
+			}
+
+			// Start reading the data for the metadata block
+			isInMetadataBlock = true
+			metadataBlockCount++
+
+			continue // Do not add the character to metadataBlockContents
+		}
+
+		// End of metadata block
+		if char == ']' {
+			isInMetadataBlock = false
+
+			// Metadata contents cannot be empty, failing example: `[TimestampValue][][LogCategory]`
+			if len(metadataBlockContents) == 0 {
+				err = errorLineEmptyMetadataBlock
+
+				return
+			}
+
+			// Metadata blocks must be `][` without any other characters between them, failing example: `[Timestamp] [Severity]`
+			// The len check here is necessary to prevent index out of range panic
+			if metadataBlockCount < 3 && (!hasNext || line[i+1] != '[') { // peek next character
+				err = errorLineInvalidMetadataString
+
+				return
+			}
+
+			// Valid metadata block completion
+			switch metadataBlockCount {
+			case 1: // Timestamp
+				ts, errTS := time.Parse(timestampParseLayout, string(metadataBlockContents))
+
+				if errTS != nil {
+					err = errorLineTimestampParsing
+
+					return
+				}
+
+				lineMetadata.Timestamp = ts
+			case 2: // Severity
+				lineMetadata.Severity = string(metadataBlockContents)
+			case 3: // Category
+				lineMetadata.Category = string(metadataBlockContents)
+
+				// After the metadata, there should be a `:` character, failing example: ``
+				// The len check here is necessary to prevent index out of range panic
+				if !hasNext || line[i+1] != ':' {
+					err = errorLineMissingEndOfMetadataChar
+
+					return
+				}
+
+				// We have three metadata blocks, so we break the loop for final validation
+				break charLoop
+			}
+
+			// Reset metadataBlockContents
+			metadataBlockContents = make([]byte, 0, 64)
+
+			continue // Do not add the character to metadataBlockContents
+		}
+
+		// Append character to the contents
+		metadataBlockContents = append(metadataBlockContents, char)
+	}
+
+	// Make sure the metadata was correctly closed
+	if isInMetadataBlock {
+		err = errorLineUnexpectedEnd
+
+		return
+	}
+
+	return
+}
---
+++ server/internal/domain/logs/process_chunk_test.go
@@ -0,0 +1,224 @@
+package logs
+
+import (
+	_ "embed"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+	"go.uber.org/zap"
+)
+
+//go:embed testdata/chunk_short.log
+var shortChunk []byte
+
+// TODO(lucianonooijen): Add test cases for
+//   - having multi line log lines (so missing ts)
+//   - empty chunk
+//   - malformed chunk
+//   - benchmarking logs of various sizes
+
+func TestExtractMetadataFromChunk(t *testing.T) {
+	tests := map[string]struct {
+		input            []byte
+		start            string
+		end              string
+		lineCount        int
+		unprocessedLines []string
+		categories       map[string]int
+		severities       map[string]int
+	}{
+		"ShortFullLog": {
+			input:            shortChunk,
+			start:            "2024.11.14-16.08.23.317",
+			end:              "2024.11.14-16.08.25.652",
+			lineCount:        43,
+			unprocessedLines: []string{},
+			categories: map[string]int{
+				"LogStats":            1,
+				"LogTextureExample":   1,
+				"LogWorldMetrics":     4,
+				"LogInit":             2,
+				"LogAnalytics":        1,
+				"LogAudio":            6,
+				"LogAudioMixer":       6,
+				"SourceControl":       1,
+				"LogUnrealEdMisc":     2,
+				"Cmd":                 2,
+				"MapCheck":            1,
+				"LogWorld":            1,
+				"LogSlate":            4,
+				"LogWorldPartition":   1,
+				"LogUObjectHash":      1,
+				"LogAssetRegistry":    1,
+				"LogStall":            2,
+				"LogNetVersion":       1,
+				"LogLoad":             1,
+				"LogContentStreaming": 1,
+				"LogCsvProfiler":      1,
+				"LogCapsaCore":        1,
+				"LogLiveCoding":       1,
+			},
+			severities: map[string]int{
+				"Log":         23,
+				"Display":     17,
+				"Warning":     1,
+				"Verbose":     1,
+				"VeryVerbose": 1,
+			},
+		},
+	}
+
+	for tt, tData := range tests {
+		t.Run(tt, func(t *testing.T) {
+			expectedStart, err := time.Parse(timestampParseLayout, tData.start)
+			require.NoError(t, err)
+
+			expectedEnd, err := time.Parse(timestampParseLayout, tData.end)
+			require.NoError(t, err)
+
+			metadata, unprocessedLines := extractMetadataFromChunk(zap.NewNop().Sugar(), tData.input)
+
+			require.Equal(t, expectedStart, metadata.Start)
+			require.Equal(t, expectedEnd, metadata.End)
+			require.Equal(t, tData.lineCount, metadata.LineCount)
+			require.Equal(t, tData.categories, metadata.CategoriesCount)
+			require.Equal(t, tData.severities, metadata.SeveritiesCount)
+			require.Equal(t, tData.unprocessedLines, unprocessedLines)
+		})
+	}
+}
+
+//nolint:funlen // Long due to tests declarations
+func TestExtractMetadataFromLine(t *testing.T) {
+	tests := map[string]struct {
+		input      []byte
+		err        error
+		isComplete bool
+		timestamp  time.Time
+		severity   string
+		category   string
+	}{
+		"Valid": {
+			input:      []byte("[2024.11.14-16.08.25.609][Log][LogCapsaCore]: Capsa ID: 00000000-0000-0000-0000-000000000000 | CapsaLogURL: https://example.dev/logs/00000000-0000-0000-0000-000000000000"),
+			err:        nil,
+			isComplete: true,
+			timestamp:  time.Date(2024, 11, 14, 16, 8, 25, 609000000, time.UTC),
+			severity:   "Log",
+			category:   "LogCapsaCore",
+		},
+		"Invalid_EmptyLine": {
+			input:      []byte(""),
+			err:        errorLineEmpty,
+			isComplete: false,
+			timestamp:  time.Time{},
+			severity:   "",
+			category:   "",
+		},
+		"Invalid_IncorrectStart_Text": {
+			input:      []byte("Invalid log line"),
+			err:        errorLineNoValidStart,
+			isComplete: false,
+			timestamp:  time.Time{},
+			severity:   "",
+			category:   "",
+		},
+		"Invalid_IncorrectStart_Space": {
+			input:      []byte("    === some info continued from the previous line"),
+			err:        errorLineNoValidStart,
+			isComplete: false,
+			timestamp:  time.Time{},
+			severity:   "",
+			category:   "",
+		},
+		"Invalid_ExtraOpenBracket": {
+			input:      []byte("[2024.11.14-16.08.25.609][[Log][LogCapsaCore]: Capsa ID: 00000000-0000-0000-0000-000000000000 | CapsaLogURL: https://example.dev/logs/00000000-0000-0000-0000-000000000000"),
+			err:        errorLineInvalidMetadataChar,
+			isComplete: false,
+			timestamp:  time.Date(2024, 11, 14, 16, 8, 25, 609000000, time.UTC),
+			severity:   "",
+			category:   "",
+		},
+		"Invalid_CutOff": {
+			input:      []byte("[2024.11.14-16.08.25.609][Log][LogCaps"),
+			err:        errorLineUnexpectedEnd,
+			isComplete: false,
+			timestamp:  time.Date(2024, 11, 14, 16, 8, 25, 609000000, time.UTC),
+			severity:   "Log",
+			category:   "",
+		},
+		"Invalid_EmptyMetadata": {
+			input:      []byte("[2024.11.14-16.08.25.609][Log][]: Capsa ID: 00000000-0000-0000-0000-000000000000 | CapsaLogURL: https://example.dev/logs/00000000-0000-0000-0000-000000000000"),
+			err:        errorLineEmptyMetadataBlock,
+			isComplete: false,
+			timestamp:  time.Date(2024, 11, 14, 16, 8, 25, 609000000, time.UTC),
+			severity:   "Log",
+			category:   "",
+		},
+		"Invalid_SpacePaddingBetweenMetadata": {
+			input:      []byte("[2024.11.14-16.08.25.609][Log] [LogCapsaCore]: Capsa ID: 00000000-0000-0000-0000-000000000000 | CapsaLogURL: https://example.dev/logs/00000000-0000-0000-0000-000000000000"),
+			err:        errorLineInvalidMetadataString,
+			isComplete: false,
+			timestamp:  time.Date(2024, 11, 14, 16, 8, 25, 609000000, time.UTC),
+			severity:   "",
+			category:   "",
+		},
+		"Invalid_CharactersBetweenMetadataBlocks": {
+			input:      []byte("[2024.11.14-16.08.25.609]Log[LogCapsaCore]: Capsa ID: 00000000-0000-0000-0000-000000000000 | CapsaLogURL: https://example.dev/logs/00000000-0000-0000-0000-000000000000"),
+			err:        errorLineInvalidMetadataString,
+			isComplete: false,
+			timestamp:  time.Time{},
+			severity:   "",
+			category:   "",
+		},
+		"Invalid_IncorrectTimeFormat": {
+			input:      []byte("[2024-11-14.16-08-25-609][Log][LogCapsaCore]: Capsa ID: 00000000-0000-0000-0000-000000000000 | CapsaLogURL: https://example.dev/logs/00000000-0000-0000-0000-000000000000"),
+			err:        errorLineTimestampParsing,
+			isComplete: false,
+			timestamp:  time.Time{},
+			severity:   "",
+			category:   "",
+		},
+		"Invalid_IncorrectEndOfMetadata_MissingSemicolon": {
+			input:      []byte("[2024.11.14-16.08.25.609][Log][LogCapsaCore] Capsa ID: 00000000-0000-0000-0000-000000000000 | CapsaLogURL: https://example.dev/logs/00000000-0000-0000-0000-000000000000"),
+			err:        errorLineMissingEndOfMetadataChar,
+			isComplete: true,
+			timestamp:  time.Date(2024, 11, 14, 16, 8, 25, 609000000, time.UTC),
+			severity:   "Log",
+			category:   "LogCapsaCore",
+		},
+		"Invalid_IncorrectEndOfMetadata_SpaceBeforeSemicolon": {
+			input:      []byte("[2024.11.14-16.08.25.609][Log][LogCapsaCore] : Capsa ID: 00000000-0000-0000-0000-000000000000 | CapsaLogURL: https://example.dev/logs/00000000-0000-0000-0000-000000000000"),
+			err:        errorLineMissingEndOfMetadataChar,
+			isComplete: true,
+			timestamp:  time.Date(2024, 11, 14, 16, 8, 25, 609000000, time.UTC),
+			severity:   "Log",
+			category:   "LogCapsaCore",
+		},
+		"Invalid_IncorrectEndOfMetadata_OnlyMetadata": {
+			input:      []byte("[2024.11.14-16.08.25.609][Log][LogCapsaCore]"),
+			err:        errorLineMissingEndOfMetadataChar,
+			isComplete: true,
+			timestamp:  time.Date(2024, 11, 14, 16, 8, 25, 609000000, time.UTC),
+			severity:   "Log",
+			category:   "LogCapsaCore",
+		},
+	}
+
+	for tt, tData := range tests {
+		t.Run(tt, func(t *testing.T) {
+			got, err := extractMetadataFromLine(tData.input)
+
+			if tData.err != nil {
+				require.ErrorIs(t, tData.err, err)
+			} else {
+				require.NoError(t, err)
+			}
+
+			require.Equal(t, tData.timestamp, got.Timestamp)
+			require.Equal(t, tData.severity, got.Severity)
+			require.Equal(t, tData.category, got.Category)
+			require.Equal(t, tData.isComplete, got.isComplete())
+		})
+	}
+}
