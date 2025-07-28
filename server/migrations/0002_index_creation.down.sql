d97fd9fa21b7828e6ad1080970e76030f3503b23
Luciano Nooijen <luciano@lucianonooijen.com>
2025-07-28 18:31:37 +0200

    fix(server) wrap index migrations in transaction block

--- server/migrations/0002_index_creation.down.sql
+++ server/migrations/0002_index_creation.down.sql
@@ -1,12 +1,14 @@
--- Not adding BEGIN; COMMIT; as CONCURRENTLY cannot be used in transactions.
+BEGIN;
 
 -- Drop filtering indexes
-DROP INDEX CONCURRENTLY IF EXISTS idx_logs_log_uuid;
-DROP INDEX CONCURRENTLY IF EXISTS idx_logs_platform_logtype;
-DROP INDEX CONCURRENTLY IF EXISTS idx_environments_key;
+DROP INDEX IF EXISTS idx_logs_log_uuid;
+DROP INDEX IF EXISTS idx_logs_platform_logtype;
+DROP INDEX IF EXISTS idx_environments_key;
 
 -- Drop join indexes
-DROP INDEX CONCURRENTLY IF EXISTS idx_logs_environment;
-DROP INDEX CONCURRENTLY IF EXISTS idx_logs_chunks_log;
-DROP INDEX CONCURRENTLY IF EXISTS idx_logs_links_source;
-DROP INDEX CONCURRENTLY IF EXISTS idx_environments_title;
+DROP INDEX IF EXISTS idx_logs_environment;
+DROP INDEX IF EXISTS idx_logs_chunks_log;
+DROP INDEX IF EXISTS idx_logs_links_source;
+DROP INDEX IF EXISTS idx_environments_title;
+
+COMMIT;
--- server/migrations/0002_index_creation.up.sql
+++ server/migrations/0002_index_creation.up.sql
@@ -1,12 +1,14 @@
--- Not adding BEGIN; COMMIT; as CONCURRENTLY cannot be used in transactions.
+BEGIN;
 
 -- Filtering indexes
-CREATE INDEX CONCURRENTLY IF NOT EXISTS idx_logs_platform_logtype ON logs(platform, log_type);
-CREATE UNIQUE INDEX CONCURRENTLY IF NOT EXISTS idx_logs_log_uuid ON logs(log_uuid);
-CREATE UNIQUE INDEX CONCURRENTLY IF NOT EXISTS idx_environments_key ON environments(key);
+CREATE INDEX IF NOT EXISTS idx_logs_platform_logtype ON logs(platform, log_type);
+CREATE UNIQUE INDEX IF NOT EXISTS idx_logs_log_uuid ON logs(log_uuid);
+CREATE UNIQUE INDEX IF NOT EXISTS idx_environments_key ON environments(key);
 
 -- Join indexes
-CREATE INDEX CONCURRENTLY IF NOT EXISTS idx_logs_environment ON logs(environment);
-CREATE INDEX CONCURRENTLY IF NOT EXISTS idx_logs_chunks_log ON logs_chunks(log);
-CREATE INDEX CONCURRENTLY IF NOT EXISTS idx_logs_links_source ON logs_links(source);
-CREATE INDEX CONCURRENTLY IF NOT EXISTS idx_environments_title ON environments(title);
+CREATE INDEX IF NOT EXISTS idx_logs_environment ON logs(environment);
+CREATE INDEX IF NOT EXISTS idx_logs_chunks_log ON logs_chunks(log);
+CREATE INDEX IF NOT EXISTS idx_logs_links_source ON logs_links(source);
+CREATE INDEX IF NOT EXISTS idx_environments_title ON environments(title);
+
+COMMIT;
